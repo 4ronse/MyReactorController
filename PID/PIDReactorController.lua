9bd39dcef0b2d6cf5de2ee985c201eb722e817d7
Ron Segal <4ronse@gmail.com>
2024-02-21 22:45:01 +0200

    Testing some other shit

--- PID/PIDReactorController.lua
+++ PID/PIDReactorController.lua
@@ -9,6 +9,8 @@ function PIDReactorSteamMode.new(reactorObject)
     self.reactor = reactorObject
 
     self.last_tick = os.clock()
+    self.back_log = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }
+    self.back_log_i = 1
 
     return self
 end
@@ -21,14 +23,34 @@ function PIDReactorSteamMode.doTick(self)
     local dt = ct - self.last_tick
 
     local hotFluidLastTick = self.reactor.getHotFluidProducedLastTick()
+    self.insertToBacklog(hotFluidLastTick)
 
-    local power = 100 - self.pid:calc(10000, hotFluidLastTick, dt)
+    local power = 100 - self.pid:calc(10000, self.getAverage(), dt)
 
-    print(string.format("CT: %f\nDT: %f\nHFLT: %f\nRODS: %d -> %d\n\n", ct, dt, hotFluidLastTick,
-        self.reactor.getControlRodLevel(1), power))
+    print(string.format("CT: %f\nDT: %f\nHFLT: %f\nAVG: %f\nRODS: %d -> %d\n\n", ct, dt, hotFluidLastTick,
+        self.getAverage(), self.reactor.getControlRodLevel(1), power))
 
     self.last_tick = ct
     self.reactor.setAllControlRodLevels(power)
+end
+
+function PIDReactorSteamMode.insertToBacklog(self, v)
+    if self.back_log_i > 10 then
+        self.back_log_i = 1
+    end
+
+    self.back_log[self.back_log_i] = v
+    self.back_log_i = self.back_log_i + 1
+end
+
+function PIDReactorSteamMode.getAverage(self)
+    local sum = 0
+
+    for _, v in pairs(self.backlog) do
+        sum = sum + v
+    end
+
+    return sum / #self.backlog
 end
 
 return PIDReactorSteamMode
