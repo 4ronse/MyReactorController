3dbc39fb1606fe3521fb94ea0494609b1eba3c4f
Ron Segal <4ronse@gmail.com>
2024-02-21 14:40:33 +0200

    Lego

--- main.lua
+++ main.lua
@@ -4,4 +4,19 @@ local PIDReactorController = require "PID.PIDReactorController"
 local pid = PID.new(0.1, 0.01, 0.05)
 local reactor = peripheral.wrap("BigReactors-Reactor_3")
 local reactorController = PIDReactorController.new(reactor, pid, 10000)
-reactorController:controlReactor()
+
+local function sleep(n --[[number]])
+    local timer = os.startTimer(n)
+    repeat
+        local _, _timer = os.pullEvent("timer")
+    until timer == _timer
+end
+
+while true do
+    print(reactorController)
+    reactorController:controlReactor()
+    print()
+    print()
+
+    sleep(0.1)
+end
